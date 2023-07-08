d176b7d4fb8eb7f39eba235bd70783e20576cc9d
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-07-08 15:00:03 +0800

    feat: 表单封装开始

--- src/components/BaForm.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts" generic="T,U">
-const props = defineProps<{
-    model: T;
-    rules: U;
-}>();
-</script>
---
+++ src/components/BaForm/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts" generic="T extends object">
+const props = defineProps<{
+    model: T;
+}>();
+</script>
+
+<template>
+    <form class="be-form"></form>
+</template>
