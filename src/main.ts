83fe1ec7ede56b39495cb2cd9334ea7cfbfdb403
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-08-05 15:04:00 +0800

    fix: 删除无用代码

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue';
 import { plugins } from './plugins';
 import 'ress/dist/ress.min.css';
 import '@mdi/font/css/materialdesignicons.min.css';
-import 'vuetify/styles';
 
 const app = createApp(App);
 plugins.forEach(plugin => {
